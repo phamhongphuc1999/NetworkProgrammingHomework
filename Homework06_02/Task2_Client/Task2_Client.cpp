f8583947fc042cf5bc3349b5fc5cb3933466a6e9
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-05-08 15:04:51 +0700

    fix homework06_02

--- Homework06_02/Task2_Client/Task2_Client.cpp
+++ Homework06_02/Task2_Client/Task2_Client.cpp
@@ -44,23 +44,6 @@ char* AddHeader(char* dest, char* source) {
 	strcat_s(dest, strlen(source) + strlen(dest) + 1, source);
 	return dest;
 }
-
-//Packed with login module
-char* EncapsulateLogin(char* buff, char* username, char* password) {
-	buff[0] = '1'; buff[1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(username) + 1, username);
-	int length = strlen(buff);
-	buff[length] = ' '; buff[length + 1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(password) + 1, password);
-	return buff;
-}
-
-//package with logout module
-char* EncapsulateLogout(char* buff, char* username) {
-	buff[0] = '2'; buff[1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(username) + 1, username);
-	return buff;
-}
 #pragma endregion
 
 #pragma region STREAM TCP
@@ -93,10 +76,152 @@ int SEND_TCP(SOCKET s, char* buff, int flag) {
 	}
 	return result;
 }
+
+int ReceiveFromServer(SOCKET s, int flag) {
+	char* buff = new char[BUFF_SIZE];
+	int ret = RECEIVE_TCP(s, buff, flag);
+	if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	buff[ret] = 0;
+	if (buff[1] == '1') {
+		switch (buff[0])
+		{
+		case '1':
+			while (true) {
+				buff = new char[BUFF_SIZE];
+				ret = RECEIVE_TCP(s, buff, flag);
+				if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+				buff[ret] = 0;
+				if (!strcmp(buff, "0")) break;
+				printf("Receive from server: %s\n", buff);
+			} break;
+		case '2':
+			printf("get address infomation fall\n");
+			break;
+		case '3':
+			printf("not found infomation\n");
+			break;
+		}
+	}
+	else if (buff[1] == '0') {
+		switch (buff[0])
+		{
+		case '1':
+			ret = RECEIVE_TCP(s, buff, flag);
+			if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+			buff[ret] = 0;
+			printf("Receive from server: %s\n", buff);
+			break;
+		case '2':
+			printf("get ip infomation fall\n");
+			break;
+		case '3':
+			printf("not found infomation\n");
+			break;
+		}
+	}
+	return ret;
+}
+#pragma endregion
+
+#pragma region INPUT AND DATA
+//Check value that client send
+//return 0 if value is IP, 1 if value is domain name
+int IsDomainNameOrIP(char* value) {
+	int result = 0, index = 0, sum = 0, countPoint = 0;
+	char element = value[0];
+	while (element != '\0') {
+		if ('0' <= element && element <= '9') {
+			sum = sum * 10 + (element - '0');
+			element = value[++index];
+		}
+		else if (element == '.') {
+			countPoint++;
+			if (sum > 255 || countPoint > 3) { result = 1; break; }
+			else {
+				sum = 0; element = value[++index];
+			}
+		}
+		else {
+			result = 1; break;
+		}
+	}
+	return result;
+}
+
+bool CheckConnect(char* input, u_short* port, char* name) {
+	int index = 0, count = 0, length = strlen(input);
+	while (input[index++] == ' ' && index < length); index--;
+	while (index < length && input[index] != ' ') {
+		name[count] = input[index];
+		index++; count++;
+	}
+	name[count++] = 0; u_short i = 0;
+	while (input[index++] == ' ' && index < length); index--;
+	while (index < length && '0' <= input[index] && '9' >= input[index]) {
+		u_short temp = (u_short)(input[index] - '0');
+		i = i * 10 + temp;
+		index++;
+	}
+	*port = i;
+	return (i > 0) && !IsDomainNameOrIP(name);
+}
+
+bool IsContainSpaceOrEmpty(char* input) {
+	bool result = false;
+	int length = strlen(input);
+	if (length == 0) return true;
+	for (int i = 0; i < length; i++) {
+		if (input[i] == ' ') {
+			result = true;
+			break;
+		}
+	}
+	return result;
+}
 #pragma endregion
 
 int main()
 {
-    return 0;
-}
+	WSADATA wsaData;
+	WORD version = MAKEWORD(2, 2);
+	if (WSAStartup(version, &wsaData)) {
+		printf("version is not supported\n");
+		return 0;
+	}
+	SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int tv = 10000;
+	setsockopt(client, SOL_SOCKET, SO_RCVTIMEO, (const char*)(&tv), sizeof(int));
+moc1:
+	char* temp = new char[BUFF_SIZE];
+	char* serverAddrIpv4 = new char[BUFF_SIZE];
+	u_short serverPort = 0;
+	fflush(stdin); printf("%s: ", CLIENT_EXE);
+	gets_s(temp, BUFF_SIZE);
+	if (!CheckConnect(temp, &serverPort, serverAddrIpv4)) {
+		printf("Wrong input\n"); goto moc1;
+	}
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(serverPort);
+	serverAddr.sin_addr.s_addr = inet_addr(serverAddrIpv4);
+	if (connect(client, (sockaddr*)&serverAddr, sizeof(serverAddr))) {
+		printf("can not connect server %d", WSAGetLastError());
+		return 0;
+	}
+	printf("connected server\n");
+	while (true) {
+		char buff[BUFF_SIZE], dest[BUFF_SIZE];
+		fflush(stdin);
+		gets_s(buff);
+		int ret = SEND_TCP(client, AddHeader(dest, buff), 0);
+		if (ret == SOCKET_ERROR) printf("can not send message\n");
 
+		ret = ReceiveFromServer(client, 0);
+		if (ret == SOCKET_ERROR) {
+			printf("ERROR: %d", WSAGetLastError());
+			break;
+		}
+	}
+	closesocket(client);
+	WSACleanup();
+}
--- Homework06_02/Task2_Server/Task2_Server.cpp
+++ Homework06_02/Task2_Server/Task2_Server.cpp
@@ -21,12 +21,28 @@ using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 
 #pragma region COMMON
+/*
+    type: 1-domain name, 0-ip
+	status: 1-success, 2-get address infomation fall, 3-not found infomation
+*/
 struct SESSION {
 	int status;
 	int type;
 	addrinfo* address;
 	char* hostName;
+	SOCKET connSock;
 };
+
+int isThreadFull;
+
+void InitiateSession(SESSION* session) {
+	session->status = -1;
+	session->type = -1;
+	session->address = NULL;
+	session->hostName = new  char[BUFF_SIZE];
+	session->connSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	session->connSock = 0;
+}
 #pragma endregion
 
 #pragma region ENCAPSULATION
@@ -56,23 +72,6 @@ char* AddHeader(char* dest, char* source) {
 	dest[10] = 0;
 	strcat_s(dest, strlen(source) + strlen(dest) + 1, source);
 	return dest;
-}
-
-//Packed with login module
-char* EncapsulateLogin(char* buff, char* username, char* password) {
-	buff[0] = '1'; buff[1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(username) + 1, username);
-	int length = strlen(buff);
-	buff[length] = ' '; buff[length + 1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(password) + 1, password);
-	return buff;
-}
-
-//package with logout module
-char* EncapsulateLogout(char* buff, char* username) {
-	buff[0] = '2'; buff[1] = 0;
-	strcat_s(buff, strlen(buff) + strlen(username) + 1, username);
-	return buff;
 }
 #pragma endregion
 
@@ -106,6 +105,61 @@ int SEND_TCP(SOCKET s, char* buff, int flag) {
 	}
 	return result;
 }
+
+int SendDomainName(SESSION session, int flag) {
+	int status = session.status, ret;
+	char dest[BUFF_SIZE];
+	if (status == 1) {
+		addrinfo* addr = session.address;
+		char* c = new char[3]{ "11" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, c), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+		while (addr != NULL) {
+			sockaddr_in* addrTemp = (sockaddr_in*)addr->ai_addr;
+			char* cTemp = inet_ntoa(addrTemp->sin_addr);
+			ret = SEND_TCP(session.connSock, AddHeader(dest, cTemp), flag);
+			if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+			addr = addr->ai_next;
+		}
+		c = new char[2]{ "0" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, c), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	else if (status == 2) {
+		char* cTemp = new char[3]{ "21" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, cTemp), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	else if (status == 3) {
+		char* cTemp = new char[3]{ "31" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, cTemp), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	return 0;
+}
+
+int SendIP(SESSION session, int flag) {
+	int status = session.status, ret;
+	char dest[BUFF_SIZE];
+	if (status == 1) {
+		char* c = new char[3]{ "10" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, c), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+		ret = SEND_TCP(session.connSock, AddHeader(dest, session.hostName), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	else if (status == 2) {
+		char* cTemp = new char[3]{ "20" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, cTemp), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	else if (status == 3) {
+		char* cTemp = new char[3]{ "30" };
+		ret = SEND_TCP(session.connSock, AddHeader(dest, cTemp), flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	}
+	return 0;
+}
 #pragma endregion
 
 #pragma region CHECK MESSAGE FROM CLIENT
@@ -135,39 +189,33 @@ int IsDomainNameOrIP(char* value) {
 #pragma endregion
 
 #pragma region RESOLVER
-SESSION ResolverDomainName(char* name) {
-	addrinfo* infoHostName = NULL, *temp = NULL;
-	SESSION session;
-	session.type = 1; 
-	session.hostName = NULL;
+void ResolverDomainName(char* name, SESSION* session) {
+	addrinfo* infoHostName = NULL;
+	session->type = 1; 
+	session->hostName = NULL;
 
 	int check = getaddrinfo(name, "http", NULL, &infoHostName);
 	if (check) { //get address infomation fall
-		session.status = 2;
-		session.address = NULL;
-		return session;
+		session->status = 2;
+		session->address = NULL;
 	}
-	temp = infoHostName;
-	if (temp == NULL) { //not found infomation
+	if (infoHostName == NULL) { //not found infomation
+		session->status = 3;
+		session->address = NULL;
 		freeaddrinfo(infoHostName);
-		session.status = 3;
-		session.address = NULL;
-		return session;
 	}
 	else
 	{
+		session->status = 1;
+		session->address = infoHostName;
 		freeaddrinfo(infoHostName);
-		session.status = 1;
-		session.address = temp;
-		return session;
 	}
 }
 
-SESSION ResolverIPAddress(char* ip) {
+void ResolverIPAddress(char* ip, SESSION* session) {
 	char serverInfo[NI_MAXSERV], hostName[NI_MAXHOST];
-	SESSION session; 
-	session.address = NULL; 
-	session.type = 0;
+	session->address = NULL; 
+	session->type = 0;
 	sockaddr_in addr;
 	u_short port = 1;
 
@@ -177,23 +225,123 @@ SESSION ResolverIPAddress(char* ip) {
 	int check = getnameinfo((sockaddr*)&addr, sizeof(sockaddr), hostName, NI_MAXHOST,
 		serverInfo, NI_MAXSERV, NI_NUMERICSERV);
 	if (check) {
-		session.hostName = NULL;
-		session.status = 2;
+		session->hostName = NULL;
+		session->status = 2;
 	}
 	if (ip == NULL || !strcmp(ip, hostName)) {
-		session.hostName = NULL;
-		session.status = 3;
+		session->hostName = NULL;
+		session->status = 3;
 	}
 	else {
-		session.hostName = hostName;
-		session.status = 1;
+		session->hostName = hostName;
+		session->status = 1;
 	}
-	return session;
+}
+#pragma endregion
+
+#pragma region HANDLE MULTIPLE CLIENT
+unsigned _stdcall Handler(void* param) {
+	SOCKET listenSocket = (SOCKET)param;
+	sockaddr_in clientAddr;
+	char buff[BUFF_SIZE];
+	int ret, numberOfClient = 0, nEvents, clientAddrLen;
+	SESSION client[FD_SETSIZE];
+	SOCKET connSock;
+	for (int i = 0; i < FD_SETSIZE; i++) InitiateSession(&client[i]);
+
+	fd_set readfds, writefds;
+	FD_ZERO(&readfds); FD_ZERO(&writefds);
+	timeval timeoutInterval;
+	timeoutInterval.tv_sec = 10;
+	timeoutInterval.tv_usec = 0;
+
+	while (true)
+	{
+		FD_SET(listenSocket, &readfds);
+		for (int i = 0; i < FD_SETSIZE; i++) {
+			if (client[i].connSock > 0) FD_SET(client[i].connSock, &readfds);
+		}
+		writefds = readfds;
+		nEvents = select(0, &readfds, 0, 0, 0);
+		if (nEvents < 0) {
+			printf("ERROR: cannot poll socket: %d\n", WSAGetLastError());
+			break;
+		}
+		if (FD_ISSET(listenSocket, &readfds)) {
+			clientAddrLen = sizeof(clientAddr);
+			connSock = accept(listenSocket, (sockaddr*)&clientAddr, &clientAddrLen);
+			int i;
+			for (i = 0; i < FD_SETSIZE; i++) {
+				if (client[i].connSock <= 0) {
+					client[i].connSock = connSock;
+					break;
+				}
+			}
+			if (i == FD_SETSIZE) isThreadFull = 1;
+			if (--nEvents <= 0) continue;
+		}
+		for (int i = 0; i < FD_SETSIZE; i++) {
+			if (client[i].connSock <= 0) continue;
+			ret = RECEIVE_TCP(client[i].connSock, buff, 0);
+			if (ret <= 0) {
+				if (ret == SOCKET_ERROR) printf("Connection shutdown\n");
+				else if (ret == 0) printf("Client close connection\n");
+				closesocket(client[i].connSock);
+				InitiateSession(&client[i]);
+				continue;
+			}
+			else if (ret > 0) {
+				buff[ret] = 0;
+				if (IsDomainNameOrIP(buff)) {
+					ResolverDomainName(buff, &client[i]);
+					ret = SendDomainName(client[i], 0);
+					if (ret == SOCKET_ERROR) printf("cannot send\n");
+				}
+				else {
+					ResolverIPAddress(buff, &client[i]);
+					ret = SendIP(client[i], 0);
+					if (ret == SOCKET_ERROR) printf("cannot send\n");
+				}
+			}
+		}
+	}
+	return 0;
 }
 #pragma endregion
 
 int main()
 {
-    
+	WSADATA wsaData;
+	WORD version = MAKEWORD(2, 2);
+	if (WSAStartup(version, &wsaData)) {
+		printf("version is not supported\n");
+		return 0;
+	}
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	u_short serverPort = 0;
+node1:
+	printf("%s ", SERVER_EXE);
+	scanf_s("%d", &serverPort);
+	if (serverPort < 1) {
+		printf("wrong port\n"); goto node1;
+	}
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(serverPort);
+	serverAddr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	if (bind(listenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr))) {
+		printf("can not bind this address"); return 0;
+	}
+	if (listen(listenSocket, MAX_CLIENT)) {
+		printf("can not listen"); return 0;
+	}
+	printf("SERVER START\n");
+	isThreadFull = 1;
+	while (true) {
+		if (isThreadFull == 1) {
+			_beginthreadex(0, 0, Handler, (void*)listenSocket, 0, 0);
+			isThreadFull = 0;
+		}
+	}
 }
 

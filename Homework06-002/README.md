9f0853c6aeddc3c937f038c02ca4ce5ac5d5bb82
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-05-12 15:55:43 +0700

    fix: homework06_002

---
+++ Homework06-002/README.md
@@ -0,0 +1,5 @@
+# bài tập tuần môn lập trình mạng
+#### xem yêu cầu chi tiết của từng bài tập tuần tại [đây](https://github.com/phamhongphuc1999/Lap_trinh_mang/tree/master/requiment)
+#### xem mã code minh họa tại [đây](https://github.com/phamhongphuc1999/Lap_trinh_mang/tree/master/ma-minh-hoa)
+
+<img src="https://kmin.edu.vn/assets/news/Z1.png">
---
+++ Homework06-002/Task2_Client/Task2_Client.cpp
@@ -0,0 +1,215 @@
+// Task2_Client.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <conio.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <SDKDDKVer.h>
+#include <process.h>
+
+#define BUFF_SIZE 2048
+#define CLIENT_EXE "Task2_Client.exe"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#pragma region ENCAPSULATION
+//return the message size
+int LENGTH(char* buff) {
+	int result = 0, index = 9;
+	while (!('0' <= buff[index] && buff[index] <= '9')) { index--; }
+	for (int i = index; i >= 0; i--) {
+		result = result * 10 + (buff[i] - '0');
+	}
+	return result;
+}
+
+//add message size in the top of message
+char* AddHeader(char* source) {
+	char* dest = new char[BUFF_SIZE];
+	int a = strlen(source);
+	int index = 0;
+	while (a > 0) {
+		int temp = a % 10;
+		a = a / 10;
+		dest[index] = temp + '0';
+		index++;
+	}
+	for (int i = index; i < 10; i++) {
+		dest[i] = 'e';
+	}
+	dest[10] = 0;
+	strcat_s(dest, strlen(source) + strlen(dest) + 1, source);
+	return dest;
+}
+#pragma endregion
+
+#pragma region STREAM TCP
+int RECEIVE_TCP(SOCKET s, char* buff, int flag) {
+	int index = 0, ret, result = 0;
+	char* temp = new char[10];
+	ret = recv(s, temp, 10, flag);
+	if (ret == 0) return 0;
+	else if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+	else {
+		int length = LENGTH(temp);
+		while (length > 0) {
+			ret = recv(s, &buff[index], length, 0);
+			if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+			else result += ret;
+			index += ret;
+			length -= ret;
+		}
+		return result;
+	}
+}
+
+int SEND_TCP(SOCKET s, char* buff, int flag) {
+	int nLeft = strlen(buff), index = 0, ret, result = 0;
+	while (nLeft > 0) {
+		ret = send(s, &buff[index], nLeft, flag);
+		if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+		else result += ret;
+		nLeft -= ret; index += ret;
+	}
+	return result;
+}
+#pragma endregion
+
+#pragma region INPUT AND DATA
+//Check value that client send
+//return 0 if value is IP, 1 if value is domain name
+int IsDomainNameOrIP(char* value) {
+	int result = 0, index = 0, sum = 0, countPoint = 0;
+	char element = value[0];
+	while (element != '\0') {
+		if ('0' <= element && element <= '9') {
+			sum = sum * 10 + (element - '0');
+			element = value[++index];
+		}
+		else if (element == '.') {
+			countPoint++;
+			if (sum > 255 || countPoint > 3) { result = 1; break; }
+			else {
+				sum = 0; element = value[++index];
+			}
+		}
+		else {
+			result = 1; break;
+		}
+	}
+	return result;
+}
+
+bool CheckConnect(char* input, u_short* port, char* name) {
+	int index = 0, count = 0, length = strlen(input);
+	while (input[index++] == ' ' && index < length); index--;
+	while (index < length && input[index] != ' ') {
+		name[count] = input[index];
+		index++; count++;
+	}
+	name[count++] = 0; u_short i = 0;
+	while (input[index++] == ' ' && index < length); index--;
+	while (index < length && '0' <= input[index] && '9' >= input[index]) {
+		u_short temp = (u_short)(input[index] - '0');
+		i = i * 10 + temp;
+		index++;
+	}
+	*port = i;
+	return (i > 0) && !IsDomainNameOrIP(name);
+}
+
+bool IsContainSpaceOrEmpty(char* input) {
+	bool result = false;
+	int length = strlen(input);
+	if (length == 0) return true;
+	for (int i = 0; i < length; i++) {
+		if (input[i] == ' ') {
+			result = true;
+			break;
+		}
+	}
+	return result;
+}
+#pragma endregion
+
+int main()
+{
+	WSADATA wsaData;
+	WORD version = MAKEWORD(2, 2);
+	if (WSAStartup(version, &wsaData)) {
+		printf("version is not supported\n");
+		return 0;
+	}
+	SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int tv = 10000;
+	setsockopt(client, SOL_SOCKET, SO_RCVTIMEO, (const char*)(&tv), sizeof(int));
+moc1:
+	char* temp = new char[BUFF_SIZE];
+	char* serverAddrIpv4 = new char[BUFF_SIZE];
+	u_short serverPort = 0;
+	fflush(stdin); printf("%s: ", CLIENT_EXE);
+	gets_s(temp, BUFF_SIZE);
+	if (!CheckConnect(temp, &serverPort, serverAddrIpv4)) {
+		printf("Wrong input\n"); goto moc1;
+	}
+	sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(serverPort);
+	serverAddr.sin_addr.s_addr = inet_addr(serverAddrIpv4);
+	if (connect(client, (sockaddr*)&serverAddr, sizeof(serverAddr))) {
+		printf("can not connect server %d", WSAGetLastError());
+		return 0;
+	}
+	printf("connected server\n");
+	char buff[BUFF_SIZE], dest[BUFF_SIZE], buffReceive[BUFF_SIZE];
+	while (true) {
+		fflush(stdin);
+		gets_s(buff);
+		int ret = SEND_TCP(client, AddHeader(buff), 0);
+		if (ret == SOCKET_ERROR) printf("can not send message\n");
+
+		ret = RECEIVE_TCP(client, buff, 0);
+		if (ret == SOCKET_ERROR) printf("can not send message\n");
+		buff[ret] = 0;
+		if (buff[1] == '1') {
+			switch (buff[0]) {
+			case '1':
+				while (true) {
+					ret = RECEIVE_TCP(client, buff, 0);
+					if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+					buff[ret] = 0;
+					if (!strcmp(buff, "0")) break;
+					printf("Receive from server: %s\n", buff);
+				} break;
+			case '2':
+				printf("get address infomation fall\n");
+				break;
+			case '3':
+				printf("not found infomation\n");
+				break;
+			}
+		}
+		else if (buff[1] == '0') {
+			switch (buffReceive[0]) {
+			case '1':
+				ret = RECEIVE_TCP(client, buff, 0);
+				if (ret == SOCKET_ERROR) return SOCKET_ERROR;
+				buff[ret] = 0;
+				printf("Receive from server: %s\n", buff);
+				break;
+			case '2':
+				printf("get ip infomation fall\n");
+				break;
+			case '3':
+				printf("not found infomation\n");
+				break;
+			}
+		}
+	}
+	closesocket(client);
+	WSACleanup();
+}

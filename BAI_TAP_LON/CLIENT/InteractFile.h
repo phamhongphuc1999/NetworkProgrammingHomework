2e50be708560dc4df0b1659db8796062982333a5
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-05-26 10:51:09 +0700

    delete BAI_TAP_LON

--- BAI_TAP_LON/CLIENT/InteractFile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CONST.h"
-#include <fstream>
-#include <list>
-#include <string>
-using namespace std;
-
-bool IsFileExistOrValid(string pathToFile);
-list<string> CreatePayload(string pathToFile);
